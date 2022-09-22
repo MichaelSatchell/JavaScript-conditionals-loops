e395a758adb575823e44356ff60868c482a52d2e
Michael Satchell <michaelsatchelljr@outlook.com>
2022-09-22 10:47:01 -0700

    added additional practice code. NinjaBattle and others

--- js.js
+++ js.js
@@ -27,6 +27,8 @@ const genBuzz = (limit) => {
 
 //price listing
 
+
+
 let itemList = () => {
     
     switch(itemList) {
@@ -56,3 +58,72 @@ let itemList = () => {
   
 
 
+//log and return function for days of the week.
+
+        const classDay = (day) => {
+        if (day === 'monday' || day === 'wednesday'|| day ==='thursday') {
+            console.log('Today is ' + day + ' and we have class tonight');
+        
+        } else if (day === 'sunday' || day === 'tuesday'|| day ==='friday' || day === 'saturday') {
+            console.log(`Today is ${day} no class`);
+
+        } else {
+            console.log('input a day of the week.')
+        };
+    };
+    classDay('monday')
+    classDay('sunday')
+    classDay('hello')
+    
+
+
+
+// Objects
+    const person = {
+        _firstName: 'John',
+        _lastName: 'Doe',
+        get fullName() {
+          if (this._firstName && this._lastName){
+            return `${this._firstName} ${this._lastName}`;
+          } else {
+            return 'Missing a first name or a last name.';
+          }
+        }
+      }
+       
+      // To call the getter method: 
+      person.fullName; // 'John Doe'
+
+
+      //codecademy getters and typeof example
+      const robot = {
+        _model: '1E78V2',
+        _energyLevel: 100,
+        get energyLevel(){
+          if(typeof this._energyLevel === 'number') {
+          return 'My current energy level is ' + this._energyLevel 
+        }else{
+          return 'System malfunction: cannot retrieve energy level' 
+        }
+      
+        
+      }
+      };
+      
+      console.log(robot.energyLevel)
+      
+
+      const ninjaBattle = (ninja) => {
+            
+        if (ninja === 'Naruto' || ninja === 'Sasuke' || ninja === 'Kakashi' || ninja === 'Obito' || ninja === 'Madara') {
+            console.log(`You have selected the Legendary Ninja ${ninja}`) ;
+        } else if (ninja === 'Sakura'  || ninja === 'Sia' || ninja === 'Darui') {
+            console.log(`You have selected ${ninja}`);
+        } else {
+            console.log('Invalid Ninja Name')
+        }
+      };
+      console.log(ninjaBattle('Obito'))
+      console.log(ninjaBattle('Sakura'))
+      console.log(ninjaBattle('Micha'))
+      
